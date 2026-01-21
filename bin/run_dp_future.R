94d8353a25d185d5572a681c7bc9edcdee181988
jamedina09 <jamedina09@gmail.com>
2026-01-21 15:14:04 -0500

    update call run_dp_future

--- bin/run_dp_future.R
+++ bin/run_dp_future.R
@@ -434,6 +434,6 @@ q(status = 0)
 
 # Rscript bin/run_dp_future.R --workers 4 --cores-per-job 4 --configs "fixed data_hard data_hard_soft data_soft fixed_k50 fixed_k25 data_hard_k50 data_hard_k25" -- --DRY_RUN
 
-# Rscript bin/run_dp_future.R --workers 4 --cores-per-job 4 --configs "fixed data_hard data_hard_soft data_soft fixed_k50 fixed_k25 data_hard_k50 data_hard_k25"
+# Rscript bin/run_dp_future.R --workers 3 --cores-per-job 5 --configs "fixed data_hard data_hard_soft data_soft fixed_k50 fixed_k25 data_hard_k50 data_hard_k25"
 
 # Rscript bin/run_dp_future.R --workers 1 --cores-per-job 14 --configs "fixed"
