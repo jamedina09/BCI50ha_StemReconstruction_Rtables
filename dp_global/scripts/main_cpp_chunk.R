c3677209f41144489b8072ce0cf3cd43124f8d83
jamedina09 <jamedina09@gmail.com>
2026-03-29 23:03:21 -0500

    update amximums main script

--- dp_global/scripts/main_cpp_chunk.R
+++ dp_global/scripts/main_cpp_chunk.R
@@ -689,10 +689,10 @@ run_dp_one_group <- function(dtg, dp_max_tracks) {
         # prune controls
         # NOTE: You can always define very wide based on the parameter data you have.
         prune_hard = TRUE,
-        prune_min_growth = MAX_SHRINK_FIXED * 2.5, # very wide fixed bounds
-        prune_max_growth = MAX_GROWTH_FIXED * 1.5, # very wide fixed bounds
+        prune_min_growth = MAX_SHRINK_FIXED * 1.25, # very wide fixed bounds
+        prune_max_growth = MAX_GROWTH_FIXED * 1.25, # very wide fixed bounds
         prune_use_bio_bounds = FALSE, # use fixed prune bounds instead of biological ones
-        prune_recruit_max_dbh = RECRUIT_MAX_FIXED * 1.2, # very high recruit max dbh
+        prune_recruit_max_dbh = RECRUIT_MAX_FIXED * 1.25, # very high recruit max dbh
         prune_use_bio_recruit = FALSE, # FALSE = use prune_recruit_max_dbh instead of biological (and margin) one, TRUE, set prune_recruit_max_dbh as min(prune_recruit_max_dbh, bio_recruit_max_dbh * 1.2)
         allow_provisional_anchor = isTRUE(ALLOW_PROVISIONAL_DP_ANCHOR),
         verbose = isTRUE(DP_VERBOSE)
